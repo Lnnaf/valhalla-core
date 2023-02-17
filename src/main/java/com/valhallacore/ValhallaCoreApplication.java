295024c2da3e774f8ac6031400e6810643114af5
linh nguyen van <linh.nguyenvan@axonactive.com>
2023-02-17 14:47:10 +0700

    init base project

---
+++ src/main/java/com/valhallacore/ValhallaCoreApplication.java
@@ -0,0 +1,13 @@
+package com.valhallacore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValhallaCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValhallaCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/valhallacore/ValhallaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valhallacore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValhallaCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
